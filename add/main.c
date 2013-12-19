66d4b4ae7b34235e0c27882b69a9a0208fc8a289
Baptiste Daroussin <bapt@FreeBSD.org>
2013-12-19 08:52:15 +0100

    Remove now dead code

--- add/main.c
+++ add/main.c
@@ -50,63 +50,6 @@ add_mode_t AddMode	= NORMAL;
 
 char	**pkgs;
 
-struct {
-	int lowver;	/* Lowest version number to match */
-	int hiver;	/* Highest version number to match */
-	const char *directory;	/* Directory it lives in */
-} releases[] = {
-	{ 410000, 410000, "/packages-4.1-release" },
-	{ 420000, 420000, "/packages-4.2-release" },
-	{ 430000, 430000, "/packages-4.3-release" },
-	{ 440000, 440000, "/packages-4.4-release" },
-	{ 450000, 450000, "/packages-4.5-release" },
-	{ 460000, 460001, "/packages-4.6-release" },
-	{ 460002, 460099, "/packages-4.6.2-release" },
-	{ 470000, 470099, "/packages-4.7-release" },
-	{ 480000, 480099, "/packages-4.8-release" },
-	{ 490000, 490099, "/packages-4.9-release" },
-	{ 491000, 491099, "/packages-4.10-release" },
-	{ 492000, 492099, "/packages-4.11-release" },
-	{ 500000, 500099, "/packages-5.0-release" },
-	{ 501000, 501099, "/packages-5.1-release" },
-	{ 502000, 502009, "/packages-5.2-release" },
-	{ 502010, 502099, "/packages-5.2.1-release" },
-	{ 503000, 503099, "/packages-5.3-release" },
-	{ 504000, 504099, "/packages-5.4-release" },
-	{ 505000, 505099, "/packages-5.5-release" },
-	{ 600000, 600099, "/packages-6.0-release" },
-	{ 601000, 601099, "/packages-6.1-release" },
-	{ 602000, 602099, "/packages-6.2-release" },
-	{ 603000, 603099, "/packages-6.3-release" },
-	{ 604000, 604099, "/packages-6.4-release" },
-	{ 700000, 700099, "/packages-7.0-release" },
-	{ 701000, 701099, "/packages-7.1-release" },
-	{ 702000, 702099, "/packages-7.2-release" },
-	{ 703000, 703099, "/packages-7.3-release" },
-	{ 704000, 704099, "/packages-7.4-release" },
-	{ 800000, 800499, "/packages-8.0-release" },
-	{ 801000, 801499, "/packages-8.1-release" },
-	{ 802000, 802499, "/packages-8.2-release" },
-	{ 803000, 803499, "/packages-8.3-release" },
-	{ 900000, 900499, "/packages-9.0-release" },
-	{ 901000, 901499, "/packages-9.1-release" },
-	{ 902000, 902499, "/packages-9.2-release" },
-	{ 300000, 399000, "/packages-3-stable" },
-	{ 400000, 499000, "/packages-4-stable" },
-	{ 502100, 502128, "/packages-5-current" },
-	{ 503100, 599000, "/packages-5-stable" },
-	{ 600100, 699000, "/packages-6-stable" },
-	{ 700100, 799000, "/packages-7-stable" },
-	{ 800500, 899000, "/packages-8-stable" },
-	{ 900500, 999000, "/packages-9-stable" },
-	{ 1000000, 1099000, "/packages-10-current" },
-	{ 0, 9999999, "/packages-current" },
-	{ 0, 0, NULL }
-};
-
-static char *getpackagesite(void);
-int getosreldate(void);
-
 static void usage(void);
 
 static char opts[] = "hviIRfFnrp:P:SMt:C:K";
@@ -288,59 +231,6 @@ main(int argc, char **argv)
     }
     else
 	return 0;
-}
-
-static char *
-getpackagesite(void)
-{
-    int reldate, i;
-    static char sitepath[MAXPATHLEN];
-    int archmib[] = { CTL_HW, HW_MACHINE_ARCH };
-    char arch[64];
-    size_t archlen = sizeof(arch);
-
-    if (getenv("PACKAGESITE")) {
-	if (strlcpy(sitepath, getenv("PACKAGESITE"), sizeof(sitepath))
-	    >= sizeof(sitepath))
-	    return NULL;
-	return sitepath;
-    }
-
-    if (getenv("PACKAGEROOT")) {
-	if (strlcpy(sitepath, getenv("PACKAGEROOT"), sizeof(sitepath))
-	    >= sizeof(sitepath))
-	    return NULL;
-    } else {
-	if (strlcat(sitepath, "ftp://ftp.freebsd.org", sizeof(sitepath))
-	    >= sizeof(sitepath))
-	    return NULL;
-    }
-
-    if (strlcat(sitepath, "/pub/FreeBSD/ports/", sizeof(sitepath))
-	>= sizeof(sitepath))
-	return NULL;
-
-    if (sysctl(archmib, 2, arch, &archlen, NULL, 0) == -1)
-	return NULL;
-    arch[archlen-1] = 0;
-    if (strlcat(sitepath, arch, sizeof(sitepath)) >= sizeof(sitepath))
-	return NULL;
-
-    reldate = getosreldate();
-    for(i = 0; releases[i].directory != NULL; i++) {
-	if (reldate >= releases[i].lowver && reldate <= releases[i].hiver) {
-	    if (strlcat(sitepath, releases[i].directory, sizeof(sitepath))
-		>= sizeof(sitepath))
-		return NULL;
-	    break;
-	}
-    }
-
-    if (strlcat(sitepath, "/Latest/", sizeof(sitepath)) >= sizeof(sitepath))
-	return NULL;
-
-    return sitepath;
-
 }
 
 static void
